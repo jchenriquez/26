901a036166dd789cb8fe8d2ac606e4fa6d08de52
Jean Carlos <jhenriquez2204@gmail.com>
2019-12-04 15:11:39 +0000

    submitting a simpler solution that would also allow negative numbers.

--- main.go
+++ main.go
@@ -12,39 +12,15 @@ func removeDuplicates(nums []int) (length int) {
 		return len(nums)
 	}
 
-	var storage int
 	length = 1
-	zeroIndex := -1
-	storage |= 1
+  nextSpot := 1
 
 	for i := 1; i < len(nums); i++ {
 		if nums[i-1] != nums[i] {
 			length++
-			if nums[i] == 0 {
-				zeroIndex = i
-				continue
-			}
-			temp := 1 << i
-			storage |= temp
+			nums[nextSpot] = nums[i]
+      nextSpot++
 		}
-	}
-
-	i := 0
-	resI := 0
-
-	fmt.Printf("storage %d\n", storage)
-
-	for storage > 0 {
-		if zeroIndex >= 0 && zeroIndex <= resI {
-			nums[zeroIndex] = 0
-			resI++
-		} else if storage & 1 == 1 {
-			nums[resI] = nums[i]
-			resI++
-		}
-
-		storage = storage >> 1
-		i++
 	}
 
 	return
@@ -52,7 +28,7 @@ func removeDuplicates(nums []int) (length int) {
 }
 
 func main() {
-	arr := []int{0,0,1,1,1,2,2,3,3,4}
+	arr := []int{0,1,1,1,1,1,2,2,2,2}
 	arr = arr[:]
 	fmt.Printf("without duplicates %v, %v", removeDuplicates(arr), arr)
 }
